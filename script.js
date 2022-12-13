245bf0d1fa79c2a2eb1a96c826ac827dafd83b41
Nick Sheward <nickshwrd@gmail.com>
2022-12-13 16:08:15 +0000

    For loop to create divs with js created

---
+++ script.js
@@ -0,0 +1,11 @@
+const container = document.querySelector('#container');
+
+let htmlString = "";
+
+for (let i = 0; i <= 16; i++) {
+    htmlString += `<div class="squares">${i}</div>`;
+}
+
+container.innerHTML = htmlString;
+
+
