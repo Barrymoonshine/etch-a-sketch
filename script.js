4556406c5273a8c9d4a5b140d106c1527d534ec9
Nick Sheward <nickshwrd@gmail.com>
2022-12-13 17:11:35 +0000

    Function created for js hover background colour

--- script.js
+++ script.js
@@ -8,4 +8,9 @@ for (let i = 0; i < 256; i++) {
 
 container.innerHTML = htmlString;
 
+let squaresHover = document.querySelector(squares);
+
+squaresHover.addEventListener('mouseover', () => {
+    squaresHover.style.backgroundColor = "yellow";
+});
 
