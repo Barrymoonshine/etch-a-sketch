664e4ab2f875617d7b26ae282ea45928b94c9068
Nick Sheward <nickshwrd@gmail.com>
2022-12-15 13:05:28 +0000

    Updated createGrid function

--- script.js
+++ script.js
@@ -1,38 +1,41 @@
-const container = document.querySelector('#container');
+document.body.onload = createGrid;
 
-let htmlString = "";
+let numDivs = 1024;
 
-let gridDensity = 1024;
-
-for (let i = 0; i < gridDensity; i++) {
-    htmlString += `<div class="squares"></div>`;
+function createGrid () {
+    for (i =0; i <numDivs; i++) {
+        let newDiv = document.createElement('div');
+        newDiv.className = "squares";
+        newDiv.addEventListener('mouseover', () =>{
+            newDiv.style.backgroundColor = "grey";
+        })
+        document.getElementById('container').appendChild(newDiv);
+    } 
 }
 
-container.innerHTML = htmlString;
+// let getSquares = document.getElementsByClassName('squares');
 
-let getSquares = document.getElementsByClassName('squares');
+// for (let j = 0; j < gridDensity; j++) {
+//     getSquares[j].addEventListener('mouseover', () => {
+//         getSquares[j].style.backgroundColor = "grey";
+//     });
+// }
 
-for (let j = 0; j < gridDensity; j++) {
-    getSquares[j].addEventListener('mouseover', () => {
-        getSquares[j].style.backgroundColor = "grey";
-    });
-}
+// window.onload = function getGridSize () {
+//     let gridPerSide;
+//     let gridDensity;
+//     let el = document.getElementById('gridInput');
+//     el.onclick = function getUserInput () {
+//         let gridPerSide = prompt ('Enter the number of squares per side for the new grid');
+//         if(gridPerSide) alert (gridPerSide);
+//         let gridDensity = Math.pow(gridPerSide,2);
+//         createGrid(gridDensity);
+//     }
+// }
 
-window.onload = function getGridSize () {
-    let gridPerSide;
-    let gridDensity;
-    let el = document.getElementById('gridInput');
-    el.onclick = function getUserInput () {
-        let gridPerSide = prompt ('Enter the number of squares per side for the new grid');
-        if(gridPerSide) alert (gridPerSide);
-        let gridDensity = Math.pow(gridPerSide,2);
-        createGrid(gridDensity);
-    }
-}
-
-function createGrid (gridDensity) {
-    for (let k = 0; k < gridDensity; k++) {
-        htmlString += `<div class="squares"></div>`;
-    }
-    container.innerHTML = htmlString;
-}
+// function createGrid (gridDensity) {
+//     for (let k = 0; k < gridDensity; k++) {
+//         htmlString += `<div class="squares"></div>`;
+//     }
+//     container.innerHTML = htmlString;
+// }
