0d00c510c1a199e3c0618adabe810631222d9f1f
Nick Sheward <nickshwrd@gmail.com>
2022-12-16 11:04:35 +0000

    Created sizeDivs function

--- script.js
+++ script.js
@@ -1,27 +1,33 @@
-document.body.onload = createGrid;
+document.body.onload = createGrid(), sizeDivs();
 
 function createGrid (numDivs = 1024) {
     for (i =0; i <numDivs; i++) {
         let newDiv = document.createElement('div');
         newDiv.className = "squares";
-        newDiv.addEventListener('mouseover', () =>{
+        newDiv.addEventListener('mouseover', () => {
             newDiv.style.backgroundColor = "grey";
         })
         document.getElementById('container').appendChild(newDiv);
-    } 
-    console.log(numDivs);
+    }
 }
 
-createGrid ();
+function sizeDivs (numDivs = 1024) {
+    const allSquares = document.querySelectorAll('.squares')
+        for (const squares of allSquares) {
+            squares.style.width = (480 / (Math.sqrt(numDivs)-2)) + 'px';
+            squares.style.height = (480 / (Math.sqrt(numDivs)-2)) + 'px';
+    }   
+}
 
 document.getElementById('myBtn').addEventListener("click", resetGridSize)
 
 function resetGridSize () {
     let userInput = prompt ('Enter the number of squares per side for the new grid');
-    alert (userInput);
+    alert ('A grid size of ' + userInput + ' will be populated');
     let numDivs = Math.pow(userInput,2);
     removeDivs();
     createGrid(numDivs);
+    sizeDivs (numDivs);
 }
 
 function removeDivs () {
