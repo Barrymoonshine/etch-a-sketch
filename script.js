6914635980fd3f6870c5ccb26c1e6ef07075b779
Nick Sheward <nickshwrd@gmail.com>
2022-12-30 10:49:47 +0000

    Created functions to add event listeners using a loop and further tidied up script.js file

--- script.js
+++ script.js
@@ -1,81 +1,97 @@
-document.body.onload = createGrid(), sizeDivs();
+const container = document.getElementById('container');
+const allSquares = document.getElementsByClassName('squares');
+const ctrlBtns = document.getElementsByClassName('controlBtns');
+const getUserChoice = function (e) {
+    applyUserChoice(e.target.id);
+}
 
-function createGrid (numDivs = 1024) {
-    for (i =0; i < numDivs; i++) {
-        let newDiv = document.createElement('div');
-        newDiv.className = "squares";
-        newDiv.style.border = "1px solid lightgrey"
-        newDiv.style.backgroundColor = "white"
-        newDiv.addEventListener('mouseover', () => {
-            newDiv.style.backgroundColor = "grey";
+document.body.onload = createGrid(), sizeSquares();
+
+function createGrid(numSquares = 1024) {
+    for (i = 0; i < numSquares; i++) {
+        let newSquare = document.createElement('div');
+        newSquare.className = "squares";
+        newSquare.style.border = "1px solid lightgrey"
+        newSquare.style.backgroundColor = "white"
+        newSquare.addEventListener('mouseover', () => {
+            newSquare.style.backgroundColor = "grey";
         })
-        document.getElementById('container').appendChild(newDiv);
+        document.getElementById('container').appendChild(newSquare);
     }
 }
 
-function sizeDivs (numDivs = 1024) {
-    const allSquares = document.querySelectorAll('.squares')
-        for (const squares of allSquares) {
-            squares.style.width = (480 / (Math.sqrt(numDivs))) -2  + 'px';
-            squares.style.height = (480 / (Math.sqrt(numDivs))) -2  + 'px';
-    }   
+function sizeSquares(numSquares = 1024) {
+    for (const squares of allSquares) {
+        squares.style.width = (480 / (Math.sqrt(numSquares))) - 2 + 'px';
+        squares.style.height = (480 / (Math.sqrt(numSquares))) - 2 + 'px';
+    }
 }
 
-document.getElementById('gridSize').addEventListener("click", resetGridSize)
+for (i of ctrlBtns) {
+    i.addEventListener('click', getUserChoice);
+}
 
-function resetGridSize () {
-    let userInput = prompt ('Enter the number of squares per side for the new grid, please enter a number of 100 or below to stop you computer from blowing up!');
-    if (userInput <= 100)  {
-        alert ('A grid size of width ' + userInput + ' and height ' + userInput + ' will be populated');
-        let numDivs = Math.pow(userInput,2);
+function applyUserChoice(userChoice) {
+    if (userChoice == 'gridSize') {
+        resetGridSize();
+    } else if (userChoice == 'randomColor') {
+        applyRandomColor();
+    } else if (userChoice == 'shader') {
+        applyShader();
+    } else if (userChoice == 'classic') {
+        applyGreyColor();
+    } else if (userChoice == 'eraser') {
+        applyWhiteColor();
+    } else if (userChoice == 'reset') {
+        resetGrid();
+    }
+}
+
+function resetGridSize() {
+    let userInput = prompt('Enter the number of squares per side for the new grid, please enter a number of 100 or below to stop you computer from blowing up!');
+    if (userInput <= 100) {
+        alert('A grid size of width ' + userInput + ' and height ' + userInput + ' will be populated');
+        let numDivs = Math.pow(userInput, 2);
         removeDivs();
         createGrid(numDivs);
-        sizeDivs (numDivs);
+        sizeSquares(numDivs);
     }
     else {
-        alert ('BOOM! You wrecked your computer bro, better call Bill Gates!')
+        alert('BOOM! You wrecked your computer bro, better call Bill Gates!');
     }
 }
 
-document.getElementById('reset').addEventListener("click", resetGrid);
-
-function resetGrid () {
-    let numDivs = 1024;
+function resetGrid() {
+    let numSquares = 1024;
     removeDivs();
-    createGrid(numDivs);
-    sizeDivs (numDivs);
+    createGrid(numSquares);
+    sizeSquares(numSquares);
 }
 
-function removeDivs () {
-    const myNode = document.getElementById('container');
-    while (myNode.lastElementChild) {
-        myNode.removeChild(myNode.lastElementChild);
+function removeDivs() {
+    while (container.lastElementChild) {
+        container.removeChild(container.lastElementChild);
     }
 }
 
-document.getElementById('randomColor').addEventListener("click", applyRandomColor);
-
-
-function applyRandomColor () {
-    const allSquares = document.querySelectorAll('.squares');
-    removeMouseOver ();
-        for (const squares of allSquares) {
-            squares.onmouseover = function () {
-                squares.style.opacity = "initial";
-                squares.style.backgroundColor = randomRgbColor() ;
-            }
+function applyRandomColor() {
+    removeMouseOver();
+    for (const squares of allSquares) {
+        squares.onmouseover = function () {
+            squares.style.opacity = "initial";
+            squares.style.backgroundColor = randomRgbColor();
         }
+    }
 }
 
 function removeMouseOver() {
-    let divs = document.body.getElementsByTagName('div');
-    for (let i = 0; i < divs.length; i++) {
-        divs[i].onmouseover = function() {};
+    for (let i = 0; i < allSquares.length; i++) {
+        allSquares[i].onmouseover = function () { };
     }
 }
 
 function randomInteger(max) {
-    return Math.floor(Math.random()*(max + 1));
+    return Math.floor(Math.random() * (max + 1));
 }
 
 function randomRgbColor() {
@@ -85,45 +101,34 @@ function randomRgbColor() {
     return `rgb(${r},${g},${b})`;
 }
 
-document.getElementById('shader').addEventListener("click", applyShader);
-
-
-function applyShader () {
-    removeMouseOver ();
-    const allSquares = document.querySelectorAll('.squares')
-        for (const squares of allSquares) {
-            squares.onmouseover = function () {
-                let opacity = Number((squares.style.opacity));
-                squares.style.backgroundColor = "black";
-                squares.style.opacity = (parseFloat(squares.style.opacity) || 0) + 0.1;
-            }
-        }
-}
-
-document.getElementById('classic').addEventListener("click", applyGreyColor);
-
-function applyGreyColor () {
-   
-    const allSquares = document.querySelectorAll('.squares')
-        for (const squares of allSquares) {
-            squares.onmouseover = function () {
-                 squares.style.opacity = "initial";
-                 squares.style.backgroundColor = "grey";
-                }
-        }
-}
-
-document.getElementById('eraser').addEventListener("click", applyWhiteColor);
-
-function applyWhiteColor () {
-    removeMouseOver ();
-    const allSquares = document.querySelectorAll('.squares')
-        for (const squares of allSquares) {
-            squares.onmouseover = function () {
-                squares.style.opacity = "initial";
-                squares.style.backgroundColor = "white";
-                squares.style.border = "1px solid lightgrey";
+function applyShader() {
+    removeMouseOver();
+    for (const squares of allSquares) {
+        squares.onmouseover = function () {
+            let opacity = Number((squares.style.opacity));
+            squares.style.backgroundColor = "black";
+            squares.style.opacity = (parseFloat(squares.style.opacity) || 0) + 0.1;
         }
     }
 }
 
+function applyGreyColor() {
+    removeMouseOver();
+    for (const squares of allSquares) {
+        squares.onmouseover = function () {
+            squares.style.opacity = "initial";
+            squares.style.backgroundColor = "grey";
+        }
+    }
+}
+
+function applyWhiteColor() {
+    removeMouseOver();
+    for (const squares of allSquares) {
+        squares.onmouseover = function () {
+            squares.style.opacity = "initial";
+            squares.style.backgroundColor = "white";
+            squares.style.border = "1px solid lightgrey";
+        }
+    }
+}
